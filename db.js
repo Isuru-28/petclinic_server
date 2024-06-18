571f590b8d6aba587ab84cd079ebedb72ea1cde3
Isuru Senanayake <isurusenanayake28@gmail.com>
2024-06-18 15:47:15 +0530

    backend_commit

---
+++ db.js
@@ -0,0 +1,39 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Scooby2012',
+//     database: 'petclinic',
+// });
+
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//       console.error('Database connection failed:', err);
+//     } else {
+//       console.log('Database connected successfully');
+//       connection.release();
+//     }
+//   });
+
+// module.exports = pool.promise();
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'srv1327.hstgr.io',
+    user: 'u323893650_isuru',
+    password: 'Scooby2012#',
+    database: 'u323893650_petclinic',
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Database connection failed:', err);
+    } else {
+      console.log('Database connected successfully');
+      connection.release();
+    }
+  });
+
+module.exports = pool.promise();
