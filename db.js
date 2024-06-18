fcd7ee157ede2dc3c30d7315da61d578b9344980
Isuru Senanayake <isurusenanayake28@gmail.com>
2024-06-18 15:59:58 +0530

    backend_hosting

--- db.js
+++ db.js
@@ -19,12 +19,20 @@
 // module.exports = pool.promise();
 
 const mysql = require('mysql2');
+require("dotenv").config();
+
+// const pool = mysql.createPool({
+//     host: 'srv1327.hstgr.io',
+//     user: 'u323893650_isuru',
+//     password: 'Scooby2012#',
+//     database: 'u323893650_petclinic',
+// });
 
 const pool = mysql.createPool({
-    host: 'srv1327.hstgr.io',
-    user: 'u323893650_isuru',
-    password: 'Scooby2012#',
-    database: 'u323893650_petclinic',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 pool.getConnection((err, connection) => {
